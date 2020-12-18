3da8ec8ef9e9b2bfdfacfa7768a8c88b30ce0e32
taylor <thowell@stanford.edu>
2020-12-17 23:48:40 -0800

    works with different time step

--- benchmarks/hopper/mpc_hopper_vertical.jl
+++ benchmarks/hopper/mpc_hopper_vertical.jl
@@ -18,11 +18,13 @@ include(joinpath(@__DIR__, "hopper.jl"))
 
 n, m = size(model)
 
+x_proj = [x_proj..., x_proj[2:end]...]
+u_proj = [u_proj..., u_proj...]
 # Horizon
 T = length(x_proj)
 
 # Time step
-tf = sum(h̄)
+tf = h̄[1] * (T - 1)
 h = h̄[1]
 t = [range(0, stop = tf, length = T)...]
 
@@ -33,11 +35,11 @@ xT = [x_proj[T]; 0.0]
 X0 = [[x_proj[t]; 0.0] for t = 1:T]
 U0 = [[u_proj[t][1:5] for t = 1:T-1]..., u_proj[end][1:5]]
 
-Q = Diagonal(1.0 * @SVector ones(n))
+Q = Diagonal(10.0 * @SVector ones(n))
 R = Diagonal(1.0 * @SVector ones(m))
 # obj = LQRObjective(Q, R, 1.0 * Q, xT, T)
 Z0_track = TO.Traj([KnotPoint(X0[k], U0[k], h, t[k]) for k = 1:T])
-obj = TO.TrackingObjective(Q, R, Z0_track, Qf = 10.0 * Q)
+obj = TO.TrackingObjective(Q, R, Z0_track, Qf = 1.0 * Q)
 
 # Constraints
 include(joinpath(@__DIR__, "contact_constraints.jl"))
@@ -48,7 +50,7 @@ add_constraint!(cons, BoundConstraint(n, m,
     x_min = [model.qL; model.qL; 0.0],
     x_max = [model.qU; model.qU; Inf],
     u_min = [-100.0 * ones(model.nu); zeros(m - model.nu)],
-    u_max = [100.0 * ones(model.nu); Inf * ones(m - model.nu)]), 1:T-1)
+    u_max = [100.0 * ones(model.nu); Inf * ones(m - model.nu)]), 1:T)
 add_constraint!(cons, SD(n, model.nc, model), 1:T)
 add_constraint!(cons, IC(n, model.nc, model), 2:T)
 add_constraint!(cons, FC(m, model.nc, model), 1:T-1)
@@ -75,7 +77,7 @@ prob = TO.Problem(model, obj, xT, tf,
 # FIRST PROCESSING
 solver = ALTROSolver(prob, opts, verbose = 2)
 cost(solver)           # initial cost
-@time solve!(solver)   # solve with ALTRO
+# @time solve!(solver)   # solve with ALTRO
 max_violation(solver)  # max constraint violation
 cost(solver)           # final cost
 iterations(solver)     # total number of iterations
@@ -122,10 +124,10 @@ end
 Z_track = TO.Traj([TO.get_trajectory(solver)[1:end]...,
     TO.get_trajectory(solver)[2:end]...,
     TO.get_trajectory(solver)[2:end]...,
-    TO.get_trajectory(solver)[2:end]...,
-	TO.get_trajectory(solver)[2:end]...,
-	TO.get_trajectory(solver)[2:end]...,
-	TO.get_trajectory(solver)[2:end]...,
+    # TO.get_trajectory(solver)[2:end]...,
+	# TO.get_trajectory(solver)[2:end]...,
+	# TO.get_trajectory(solver)[2:end]...,
+	# TO.get_trajectory(solver)[2:end]...,
     TO.get_trajectory(solver)[2:end]...,
 	TO.get_trajectory(solver)[2:end]...,
     TO.get_trajectory(solver)[2:end]...])
@@ -133,10 +135,10 @@ Z_track = TO.Traj([TO.get_trajectory(solver)[1:end]...,
 Z_track_shift = TO.Traj([Z_shift[1:end]...,
     Z_shift[2:end]...,
     Z_shift[2:end]...,
-    Z_shift[2:end]...,
-	Z_shift[2:end]...,
-	Z_shift[2:end]...,
-	Z_shift[2:end]...,
+    # Z_shift[2:end]...,
+	# Z_shift[2:end]...,
+	# Z_shift[2:end]...,
+	# Z_shift[2:end]...,
     Z_shift[2:end]...,
 	Z_shift[2:end]...,
     Z_shift[2:end]...])
@@ -163,7 +165,7 @@ visualize!(vis, model, state_to_configuration(X_track, model.nq), Δt = h)
 
 ## Model-predictive control tracking problem
 include("../mpc.jl")
-push_dist = 0.0
+push_dist = 100.0
 index_dist = 101
 function run_hopper_MPC(prob_mpc, opts_mpc, Z_track;
                             num_iters = length(Z_track) - prob_mpc.N,
@@ -183,6 +185,13 @@ function run_hopper_MPC(prob_mpc, opts_mpc, Z_track;
     k_mpc = 1
     x0 = SVector(prob_mpc.x0)
     X_traj = [copy(x0) for k = 1:num_iters+1]
+	U_traj = [zeros(m) for k = 1:num_iters]
+
+	q_sim = [x0[1:model.nq], x0[model.nq .+ (1:model.nq)]]
+	v_sim = [(q_sim[end] - q_sim[end-1]) / dt]
+
+	d = 10
+	h = dt / convert(Float64, d)
 
     # Begin the MPC LOOP
     for i = 1:num_iters
@@ -192,24 +201,39 @@ function run_hopper_MPC(prob_mpc, opts_mpc, Z_track;
         TO.set_initial_time!(prob_mpc, t0)
 
         # Update initial state by using 1st control, and adding some noise
-        # x0 = discrete_dynamics(TO.integration(prob_mpc),
-        #                             prob_mpc.model, prob_mpc.Z[1])
-		w0 = (i == index_dist ? push_dist * [1.0; 0.0; 0.0; 0.0] : 1.0e-5 * randn(model.nq))
-		x0 = [step_contact(model_sim,
-			state(prob_mpc.Z[1])[1:2 * model.nq],
-			control(prob_mpc.Z[1])[1:model.nu], w0, dt); 0.0]
+
+		w0 = (i == index_dist ? push_dist * [1.0; 0.0; 0.0; 0.0] : 0.0 * randn(model.nq))
+		u0 = control(prob_mpc.Z[1])[1:model.nu]
+		U_traj[i] = u0
+
+		for i = 1:d
+		   q = step_contact(model_sim,
+			   v_sim[end], q_sim[end-1], q_sim[end],
+			   u0, w0, h)
+		   v = (q - q_sim[end]) / h
+
+		   push!(q_sim, q)
+		   push!(v_sim, v)
+		end
+
+		# x0 = [step_contact(model_sim,
+		# 	state(prob_mpc.Z[1])[1:2 * model.nq],
+		# 	control(prob_mpc.Z[1])[1:model.nu], w0, dt); 0.0]
+
+		x0 = [state(prob_mpc.Z[1])[model.nq .+ (1:model.nq)];
+			q_sim[end]; zeros(model.nc)]
 
         # Update the initial state after the dynamics are propogated.
         TO.set_initial_state!(prob_mpc, x0)
         X_traj[i+1] = x0
 
         # Update tracking cost
-		if i >= index_dist
-			TO.update_trajectory!(prob_mpc.obj, Z_track_shift, k_mpc)
-		else
-        	TO.update_trajectory!(prob_mpc.obj, Z_track, k_mpc)
-		end
-		# TO.update_trajectory!(prob_mpc.obj, Z_track, k_mpc)
+		# if i >= index_dist
+		# 	TO.update_trajectory!(prob_mpc.obj, Z_track_shift, k_mpc)
+		# else
+        # 	TO.update_trajectory!(prob_mpc.obj, Z_track, k_mpc)
+		# end
+		TO.update_trajectory!(prob_mpc.obj, Z_track, k_mpc)
 
         # Shift the initial trajectory
         RD.shift_fill!(prob_mpc.Z)
@@ -233,28 +257,28 @@ function run_hopper_MPC(prob_mpc, opts_mpc, Z_track;
     end
 
     @warn "solve success"
-    return X_traj, Dict(:time=>times, :iter=>iters)
+    return X_traj, U_traj, Dict(:time=>times, :iter=>iters)
 end
 
 Random.seed!(1)
 opts_mpc = SolverOptions(
     cost_tolerance = 1.0e-4,
     cost_tolerance_intermediate = 1.0e-4,
-    constraint_tolerance = 1.0e-2,
-    reset_duals = false,
+    constraint_tolerance = 5.0e-2,
+    reset_duals = true,
     penalty_initial = 10.0,
-    penalty_scaling = 1000.0,
+    penalty_scaling = 10000.0,
     projected_newton = false,
     iterations = 500)
 
-T_mpc = 101
+T_mpc = 201
 prob_mpc = gen_tracking_problem(prob, T_mpc,
-    Q = Diagonal(SVector{n}([10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 0.0])),
-    R = Diagonal(SVector{m}([1.0, 1.0, 1.0, 1.0, 1.0])),
-    Qf = Diagonal(SVector{n}([10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 0.0])))
-N_mpc = 900
-X_traj, res = run_hopper_MPC(prob_mpc, opts_mpc, Z_track,
-	num_iters = N_mpc, verbose = true)
+    Q = Diagonal(SVector{n}(100.0 * [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0])),
+    R = Diagonal(SVector{m}([100.0, 100.0, 1.0, 1.0, 1.0])),
+    Qf = Diagonal(SVector{n}(100.0 * [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0])))
+N_mpc = 800
+X_traj, U_traj, res = run_hopper_MPC(prob_mpc, opts_mpc, Z_track,
+	num_iters = N_mpc, verbose = 1)
 
 T_stack = length(X_track)
 tf_stack = h * (T_stack - 1)
@@ -276,22 +300,27 @@ for i = 1:model.nq
 	plt = plot!(t_stack[1:N_mpc], q_track_stack[i, 1:N_mpc],
 	    labels = "", legend = :topright,
 	    width = 2.0, color = colors[i], linestyle = :dash)
-	plt = plot!(t_stack[index_dist:N_mpc], q_track_shift_stack[i, index_dist:N_mpc],
-	    labels = "", legend = :topright,
-	    width = 2.0, color = colors[i], linestyle = :dash)
+	# plt = plot!(t_stack[index_dist:N_mpc], q_track_shift_stack[i, index_dist:N_mpc],
+	#     labels = "", legend = :topright,
+	#     width = 2.0, color = colors[i], linestyle = :dash)
 	plt = plot!(t_stack[1:N_mpc], q_traj_stack[i, :],
-	    labels = labels[i], legend = :topright,
+	    labels = labels[i], legend = :bottomleft,
 	    width = 2.0, color = colors[i])
 end
 
-plot!(t_stack[101] * ones(100),
-	range(-1.0, stop = 1.0, length = 100),
-	color = :black, label = "",
-	linestyle = :dash,
-	ylims = (minimum(q_traj_stack), maximum(q_traj_stack)),
-	width = 2.0)
+# plot!(t_stack[101] * ones(100),
+# 	range(-1.0, stop = 1.0, length = 100),
+# 	color = :black, label = "",
+# 	linestyle = :dash,
+# 	ylims = (minimum(q_traj_stack), maximum(q_traj_stack)),
+# 	width = 2.0)
 display(plt)
 
+plot(hcat(U_track[1:N_mpc]...)[1:2, :]',
+	linetype = :steppost, width = 2.0, color = :red)
+plot!(hcat(U_traj[1:N_mpc]...)[1:2, :]', linetype = :steppost,
+	width = 1.0, color = :black)
+
 vis = Visualizer()
-open(vis)
+render(vis)
 visualize!(vis, model, state_to_configuration(X_traj, model.nq), Δt = h)
--- benchmarks/hopper/simulator.jl
+++ benchmarks/hopper/simulator.jl
@@ -2,62 +2,64 @@ include("/home/taylor/Research/DirectMotionPlanning/src/DirectMotionPlanning.jl"
 # switch workspace
 include("/home/taylor/Research/DirectMotionPlanning/src/problem.jl")
 include("/home/taylor/Research/DirectMotionPlanning/models/hopper.jl")
+include("/home/taylor/Research/DirectMotionPlanning/examples/development/hopper_simulator.jl")
+
 include("/home/taylor/Research/DirectMotionPlanning/src/constraints/contact.jl")
 
 const DMP = Main.DirectMotionPlanning
 model_sim = model # DMP.no_slip_model(model)
-# con_contact = contact_constraints(model, 10)
 
-# Simulate contact model one step
-function step_contact(model, x1, u1, w1, h)
-    # Horizon
-    T = 2
+# # Simulate contact model one step
+# function step_contact(model, x1, u1, w1, h)
+#     # Horizon
+#     T = 2
+#
+#     # Objective
+#     obj = DMP.PenaltyObjective(1.0e5, model.m)
+#
+#     # Constraints
+#     con_dynamics = DMP.dynamics_constraints(model, T; w = [w1])
+#     con_contact = contact_constraints(model, T)
+#     con = DMP.multiple_constraints([con_dynamics, con_contact])
+#
+#     # Bounds
+#     _uu = Inf * ones(model.m)
+#     _uu[model.idx_u] .= u1
+#     _ul = zeros(model.m)
+#     _ul[model.idx_u] .= u1
+#     ul, uu = DMP.control_bounds(model, T, _ul, _uu)
+#
+#     xl, xu = DMP.state_bounds(model, T, x1 = x1)
+#
+#     # Problem
+#     prob = DMP.trajectory_optimization_problem(model,
+#                    obj,
+#                    T,
+#                    h = h,
+#                    xl = xl,
+#                    xu = xu,
+#                    ul = ul,
+#                    uu = uu,
+#                    con = con,
+#                    dynamics = false) # instead, encode dynamics w/ noise
+#
+#     # Trajectory initialization
+#     x0 = [x1 for t = 1:T]
+#     u0 = [[u1; 1.0e-5 * rand(model.m - model.nu)] for t = 1:T-1] # random controls
+#
+#     # Pack trajectories into vector
+#     z0 = DMP.pack(x0, u0, prob)
+#
+#     @time z = DMP.solve(prob, copy(z0), tol = 1.0e-5, c_tol = 1.0e-5)
+#
+#     @assert DMP.check_slack(z, prob) < 1.0e-4
+#     x, u = DMP.unpack(z, prob)
+#
+#     return x[end]
+# end
 
-    # Objective
-    obj = DMP.PenaltyObjective(1.0e5, model.m)
-
-    # Constraints
-    con_dynamics = DMP.dynamics_constraints(model, T; w = [w1])
-    con_contact = contact_constraints(model, T)
-    con = DMP.multiple_constraints([con_dynamics, con_contact])
-
-    # Bounds
-    _uu = Inf * ones(model.m)
-    _uu[model.idx_u] .= u1
-    _ul = zeros(model.m)
-    _ul[model.idx_u] .= u1
-    ul, uu = DMP.control_bounds(model, T, _ul, _uu)
-
-    xl, xu = DMP.state_bounds(model, T, x1 = x1)
-
-    # Problem
-    prob = DMP.trajectory_optimization_problem(model,
-                   obj,
-                   T,
-                   h = h,
-                   xl = xl,
-                   xu = xu,
-                   ul = ul,
-                   uu = uu,
-                   con = con,
-                   dynamics = false) # instead, encode dynamics w/ noise
-
-    # Trajectory initialization
-    x0 = [x1 for t = 1:T]
-    u0 = [[u1; 1.0e-5 * rand(model.m - model.nu)] for t = 1:T-1] # random controls
-
-    # Pack trajectories into vector
-    z0 = DMP.pack(x0, u0, prob)
-
-    @time z = DMP.solve(prob, copy(z0), tol = 1.0e-5, c_tol = 1.0e-5)
-
-    @assert DMP.check_slack(z, prob) < 1.0e-4
-    x, u = DMP.unpack(z, prob)
-
-    return x[end]
-end
-
-step_contact(model_sim, rand(model_sim.n), rand(model_sim.m)[1:model.nu], zeros(model_sim.nq), 0.01)
+step_contact(model_sim, rand(model_sim.nq), rand(model_sim.nq), rand(model_sim.nq),
+    rand(model_sim.m)[1:model.nu], zeros(model_sim.nq), 0.01)
 
 # x1 = rand(model_sim.n)
 # u1 = rand(model_sim.m)[1:model.nu]
